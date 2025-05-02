46e58964fb08a2b25b5cefe1a382e5b480aa5310
arina2938 <146789419+arina2938@users.noreply.github.com>
2025-05-03 00:18:35 +0300

    add env and implement env usage

--- app/database.py
+++ app/database.py
@@ -6,8 +6,9 @@ from contextlib import contextmanager
 # Сторонние библиотеки
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from config import settings
 
-DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL = settings.database_url
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
