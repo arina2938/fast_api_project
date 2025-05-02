067ddc0bddc63be369d1dd9c6e66ab628d7e44a6
arina2938 <146789419+arina2938@users.noreply.github.com>
2025-05-02 19:52:38 +0300

    add routers

--- app/core/main.py
+++ app/core/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-#from app.models import Base, engine
 from app.core.database import init_database
-from app.routers import user
 from app.routers import auth_router
-
+from app.routers import concert
+from app.routers import composer_route
+from app.routers import instruments_router
 init_database()
 app = FastAPI()
 
@@ -19,3 +19,6 @@ async def say_hello(name: str):
 
 #app.include_router(user.router)
 app.include_router(auth_router.router)
+app.include_router(concert.router)
+app.include_router(composer_route.router)
+app.include_router(instruments_router.router)
