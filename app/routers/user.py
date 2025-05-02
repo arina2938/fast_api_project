35bcdbf46901b770d95df7fffc95b8a15bd101e7
arina2938 <146789419+arina2938@users.noreply.github.com>
2025-05-02 20:05:31 +0300

    Delete unused files

--- app/routers/user.py
+++
@@ -1,5 +0,0 @@
-from fastapi import APIRouter, status, Depends, HTTPException
-from sqlalchemy.orm import Session
-from app.core.database import get_session
-from app.models.models import User
-from ..schemas import user as schema_user
