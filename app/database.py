f5d17a3ebb61228e778164ceb3be0ca9d870b1f9
arina2938 <146789419+arina2938@users.noreply.github.com>
2025-05-03 15:19:05 +0300

    delete context manager

--- app/database.py
+++ app/database.py
@@ -1,20 +1,14 @@
 """Модуль для работы с базой данных."""
 
-# Стандартные библиотеки
-from contextlib import contextmanager
-
 # Сторонние библиотеки
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from app.config import settings
 
-DATABASE_URL = settings.database_url
+DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-
-@contextmanager
 def get_session():
     """Генератор сессий базы данных.
 
