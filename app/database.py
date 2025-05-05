a588b79aca6d737ac4d6b70693c1a3466f12efbf
arina2938 <146789419+arina2938@users.noreply.github.com>
2025-05-05 12:42:55 +0300

    fix database_url
    - change constant database url to url from config

--- app/database.py
+++ app/database.py
@@ -3,8 +3,9 @@
 # Сторонние библиотеки
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from app.config import settings
 
-DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL = settings.database_url
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
